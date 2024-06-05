d87571cb606ef96113db5bad1c3ef6f7dc3848ce
Jan Svehlak <jan.svehlak@gmail.com>
2024-06-05 05:53:01 +0200

    update sripts in package

--- tests/e2e/e2e_add_and_remove.spec.ts
+++ tests/e2e/e2e_add_and_remove.spec.ts
@@ -1,9 +1,9 @@
 import {Page } from "@playwright/test";
 import test, { expect } from "@playwright/test";
 
-import productPage from "../pageObjects/productPage.page";
-import productDetailPage from "../pageObjects/productDetail.page";
-import cartPage from "../pageObjects/cartPage.page"
+import productPage from "../../pageObjects/productPage.page";
+import productDetailPage from "../../pageObjects/productDetail.page";
+import cartPage from "../../pageObjects/cartPage.page"
 
 test('Add and Remove product at product page', async({page}) => {
         
